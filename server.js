6fe260094d6888c0e7c9436fe6bd8f9508ecf625
fbrinkworth <federico.brinkworth@gmail.com>
2015-04-08 17:31:38 -0300

    Server

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var app = express();
 
+/*
 var public_dir = './www/';
 
 // routes to serve the static HTML files
 app.get('/', function(req, res) {
-    res.sendfile(public_dir + 'index.html');
+    res.render(public_dir + 'index.html');
 });
+*/
+
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html', 'css', 'js'],
+  index: "index.html",
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+  }
+};
+
+app.use(express.static('www', options));
 
 var server = app.listen(3000, function () {
 
